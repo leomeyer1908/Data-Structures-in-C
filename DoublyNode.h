d9bde602c0692f96d19ae6fd208f2e143e138bb0
Leo Meyer <>
2023-12-25 23:46:23 -0600

    Made linked list now be a doubly linked list

---
+++ DoublyNode.h
@@ -0,0 +1,16 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <stdlib.h>
+
+typedef struct DoublyNode {
+    void* value;
+    struct DoublyNode* prev;
+    struct DoublyNode* next;
+} DoublyNode;
+
+DoublyNode* getNewDoublyNode();
+
+
+
+#endif
